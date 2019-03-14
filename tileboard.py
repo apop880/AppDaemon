04d6f3e2d7e6c5c44450ac1ffb2c483997f2c4a8
apop880 <41379678+apop880@users.noreply.github.com>
2019-03-13 21:09:03 -0500

    Screensaver logic adjustments, dailyRefresh functionality
    Moved most of the logic for the screensaver into the AppDaemon script, to centralize more of the configuration and mitigate issues stemming from connection losses. Added functionality for an optional daily refresh of TileBoard.

--- tileboard.py
+++ tileboard.py
@@ -1,6 +1,7 @@
 import appdaemon.plugins.hass.hassapi as hass
 import os, os.path
 import random
+import datetime
 
 #
 # TileBoard
@@ -12,17 +13,28 @@ import random
 # tileboard:
 #   module: tileboard
 #   class: TileBoard
+#	slidesTimeout: 30
+#	dailyRefresh: true (optional)
 
 class TileBoard(hass.Hass):
 
 	def initialize(self):
 		self.throttle_timer = None
+		self.update_timer = None
 		self.listen_event(self.tb_throttle, "tb_update")
+		self.run_in(self.tb_update, 5)
+
+		#set up daily refresh listener if defined in configuration
+		if "dailyRefresh" in self.args:
+			#refresh daily at 3am
+			runtime = datetime.time(3, 0, 0)
+			self.run_daily(self.daily_refresh, runtime)
 		
 	def tb_throttle(self, event_name, data, kwargs):
 		#throttle function to ensure that we don't have a race condition
 		#will wait until webhook hasn't been called in one minute before updating slides
 		self.cancel_timer(self.throttle_timer)
+		self.cancel_timer(self.update_timer)
 		self.throttle_timer = self.run_in(self.tb_update, 60)
 
 	def tb_update(self, kwargs):
@@ -35,3 +47,7 @@ class TileBoard(hass.Hass):
 				self.slides.append(file)
 		random.shuffle(self.slides)
 		self.fire_event("tileboard", command="ss_update", slides=self.slides)
+		self.update_timer = self.run_in(self.tb_update, len(self.slides) * self.args["slidesTimeout"])
+
+	def daily_refresh(self, kwargs):
+		self.fire_event("tileboard", command="refresh")
